1ba09dad17a213d44cf44085094b3c59a114007f
Krystian Lewalski <Krystian.Lewalski@interia.pl>
2023-05-21 18:02:17 +0200

    add function to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import Form from "./Form";
+import { Form } from "./Form";
+import { useState } from "react";
+import { currencies } from "./currencies";
+
 
 function App() {
+
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+    .find (({ short }) => short === currency)
+    .rate;
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  }
+  
   return (
     <body className="body">
-    <Form />
+      <Form 
+      result={result}
+      calculateResult={calculateResult}
+      />
     </body>
   );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,40 +1,73 @@
+import { Result } from "./Result";
+import { useState } from "react";
+import { currencies } from "../currencies"
 import "./style.css";
 
-const Form = () => {
-    <form className="form">
-        <fieldset className="form__fieldset">
-            <legend className="form__legend">Kalkulator Walut</legend>
-            <p>
-                <label>
-                    <span className="form__labeltext">
-                        Kwota*:
-                    </span>
-                    <input className="form__field js-Field" name="kwota" type="number" placeholder="Podaj kwotę w PLN"
-                        autofocus step="0.01" min="0.01" required />
-                </label>
-            </p>
-            <p>
-                <label>
-                    <span className="form__labelText">
-                        Wybierz walutę**:
-                    </span>
-                    <select className="form__field js-Currency" required>
-                        <option value="">---</option>
-                        <option value="4.66">EUR</option>
-                        <option value="4.37">USD</option>
-                        <option value="4.68">CHF</option>
-                        <option value="5.27">GBP</option>
-                    </select>
-                </label>
-            </p>
-            <p>Pola wymagane oznaczone *</p>
-            <p>Kurs na dzień 21.05.2023**</p>
-            <p>
-                <button className="form__button">Oblicz</button>
-            </p>
-            <p>Wynik:<strong className="js-Converter">N/A</strong></p>
-        </fieldset>
-    </form>
+export const Form = ({ calculateResult, result }) => {
+    const [currency, setCurrency] = useState(currencies[0].short);
+    const [amount, setAmount] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(currency, amount);
+    };
+
+    return (
+        <div className="container">
+            <form className="form" onSubmit={onSubmit}>
+                <fieldset className="form__fieldset">
+                    <legend className="form__legend">
+                        Kalkulator Walut
+                    </legend>
+                    <p>
+                        <label>
+                            <span className="form__labelText">
+                                Kwota*:
+                            </span>
+                            <input
+                                value={amount}
+                                onChange={({ target }) => setAmount(target.value)}
+                                className="form__field"
+                                name="kwota"
+                                type="number"
+                                placeholder="Podaj kwotę w PLN"
+                                autofocus
+                                step="0.01"
+                                min="0.01"
+                                required
+                            />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            <span className="form__labelText">
+                                Wybierz walutę**:
+                            </span>
+                            <select className="form__field"
+                                required
+                                onChange={({ target }) => setCurrency(target.value)}
+                            >
+                                {currencies.map((currency => (
+                                    <option
+                                        key={currency.short}
+                                        value={currency.short}
+                                        >
+                                            {currency.name}
+                                        </option>
+                                )))}
+                            </select>
+                        </label>
+                    </p>
+                    <p>Pola wymagane oznaczone *</p>
+                    <p>Kurs na dzień 21.05.2023**</p>
+                    <p>
+                        <button className="form__button">Oblicz</button>
+                    </p>
+                    <Result result={result} />
+                </fieldset>
+            </form>
+        </div>
+    )
 };
 
 export default Form;
